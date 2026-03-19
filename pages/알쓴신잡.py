68c1ed42a246e660c0b9f9e29bbaf9f93018e03b
hamama11 <ydi74@sonline20.sen.go.kr>
2026-03-20 00:16:34 +0900

    Update 알쓴신잡.py

--- pages/알쓴신잡.py
+++ pages/알쓴신잡.py
@@ -7,11 +7,11 @@ url1 = "https://gemini.google.com/share/5e531d00887d"
 # 제목 + 설명
 st.markdown("""
 <p style='font-size:18px; line-height:1.7;'>
-🌌 <b>알쓴신잡 : 수학의 또 다른 세계</b><br><br>
+😶‍🌫️ <b>알쓴신잡 : 00의 또 다른 세계</b><br><br>
 우리가 미처 알지 못했던<br>
-조금 더 깊은 수학의 영역을 마주합니다.<br><br>
+조금 다른 영역을 마주한적이<br><br>
 익숙한 것들 속에도<br>
-새로운 시선과 놀라운 연결이 숨어 있습니다.
+새로운 시선과 놀라운 세계가 있다는 것을 느껴본 적 있는가?
 </p>
 """, unsafe_allow_html=True)
 
