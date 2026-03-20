6a9db97cf14aa064768ef0becb149d6e7dcdbe61
hamama11 <ydi74@sonline20.sen.go.kr>
2026-03-20 11:44:45 +0900

    Update 02_4.사인코사인법칙.py

--- pages/02_4.사인코사인법칙.py
+++ pages/02_4.사인코사인법칙.py
@@ -15,8 +15,7 @@ st.markdown("""
 </p>
 
 <p style='font-size:16px;'>
-✔ 두 변과 끼인각 → <b>코사인법칙</b><br>
-✔ 한 변과 두 각 → <b>사인법칙</b>
+✔ 두 변과 한 각 → <b>~~~법칙 ✔ 한 변과 두 각 → <b>~~~법칙</b>
 </p>
 """, unsafe_allow_html=True)
 
