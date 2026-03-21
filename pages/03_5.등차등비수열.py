89f3f7a46b3831b4b444a3dd3e687b5232df1b69
hamama11 <ydi74@sonline20.sen.go.kr>
2026-03-21 14:40:32 +0900

    Update 03_5.등차등비수열.py

--- pages/03_5.등차등비수열.py
+++ pages/03_5.등차등비수열.py
@@ -21,13 +21,13 @@ st.markdown(
 st.markdown("---")
 
 # HTML 1
-with open(BASE_DIR / "s_n,a_n.html", encoding="utf-8") as f:
+with open(BASE_DIR / "5.s_n,a_n.html", encoding="utf-8") as f:
     html1 = f.read()
 
 components.html(html1, height=700, scrolling=True)
 
 # HTML 2
-with open(BASE_DIR / "a_2n.html", encoding="utf-8") as f:
+with open(BASE_DIR / "5.a_2n.html", encoding="utf-8") as f:
     html2 = f.read()
 
 components.html(html2, height=700, scrolling=True)
