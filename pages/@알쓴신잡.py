3f13960e523b6ccd0e6d1d250310cb18e19358ab
hamama11 <ydi74@sonline20.sen.go.kr>
2026-03-26 12:49:05 +0900

    Update @알쓴신잡.py

--- pages/@알쓴신잡.py
+++ pages/@알쓴신잡.py
@@ -2,26 +2,28 @@ import streamlit as st
 import streamlit.components.v1 as components
 from pathlib import Path
 
-# 수정👉 외부 (URL)
-url1 = "https://gemini.google.com/share/5e531d00887d"
+st.set_page_config(layout="wide")
+
+# 추후 외부 URL
+url1 = "https://gemini.google.com/share/5e531000887d"
 
 # 제목 + 설명
 st.markdown("""
 <p style='font-size:18px; line-height:1.7;'>
-😶‍🌫️ <b>알쓴신잡 : 00의 또 다른 세계</b><br><br>
-우리가 미처 알지 못했던<br>
-조금 다른 영역을 마주한적이<br><br>
+<b>알쓸신잡은 언어의 또 다른 세계였고</b><br><br>
+우리가 미처 쓰지 못했지만<br>
+조금 다른 영역을 마주했던 이야기였고<br><br>
 익숙한 것들 속에도<br>
 새로운 시선과 놀라운 세계가 있다는 것을 느껴본 적 있는가?
 </p>
 """, unsafe_allow_html=True)
 
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-# 수정 👉 내부 임베딩 (로컬 HTML)
-with open(BASE_DIR / "geo_sub_thm.html", "r", encoding="utf-8") as f:
-    html = f.read()
+html_path = BASE_DIR / "geo_sub_thm.html"
 
 st.markdown("---")
 
-components.html(html, height=950, scrolling=True)
+with open(html_path, "r", encoding="utf-8") as f:
+    html1 = f.read()
+
+components.html(html1, height=1200, scrolling=True)
