e9c28da0370adf0a2b1e3dbe48fc5ee6fc45b7cc
hamama11 <ydi74@sonline20.sen.go.kr>
2026-05-12 00:37:04 +0900

    Update 01.Ⅱ-1함수의극한.py

--- pages/01.Ⅱ-1함수의극한.py
+++ pages/01.Ⅱ-1함수의극한.py
@@ -55,8 +55,8 @@ with right:
 
 st.markdown("---")
 
-# 슬라이드 5~6
-for i in range(5, 7):
+# 슬라이드 4~6
+for i in range(4, 7):
     st.image(
         f"assets/함수의극한/images/슬라이드{i}.PNG",
         use_container_width=True
