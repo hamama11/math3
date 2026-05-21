a5444ca4f439bc86b3426516788707435c5a1eba
hamama11 <ydi74@sonline20.sen.go.kr>
2026-05-21 14:38:45 +0900

    Update 00Ⅱ-3미분계수와도함수.py

--- pages/00Ⅱ-3미분계수와도함수.py
+++ pages/00Ⅱ-3미분계수와도함수.py
@@ -1,41 +1,33 @@
 import streamlit as st
 import streamlit.components.v1 as components
+left, right = st.columns([1.3, 1])
 
-st.set_page_config(layout="wide")
-# 🌌 헤더
-st.title("📈 미분계수와 도함수 : 변화율의 극한")
+with left:
+    st.markdown("""
+    <p style='font-size:19px; line-height:2;'>
 
-# 🌿 머리말
-st.markdown("""
-<p style='font-size:19px; line-height:2;'>
+    🌌 <b>변화</b><br><br>
 
-🌌 <b>변화란 과연 무엇인고?</b><br><br>
+    우리는 매 순간 달라지고 흔들리건만,<br>
+    어째서 여전히 같은 사람이라 말하는 것이더냐.<br><br>
 
-세상은 끊임없이 변하고 흐르건만,<br>
-우리는 그 움직임을 어떻게 붙잡을 수 있겠느냐.<br><br>
+    하루의 변화는 너무 작아 보이기에 스스로도 깨닫지 못하나,<br>
+    어느새 사람의 생각과 마음, 삶의 방향까지 바꾸어 놓기도 하도다.<br><br>
+    
+    두 순간 사이의 거대한 차이를 보는 것이 아니라<br>
+    <b>‘지금 이 순간, 나는 어떤 방향으로 변하고 있는가’</b><br><br>
+    
+    두려움은 변화 그 자체가 아니라,<br>
+    변화하고 있음에도 스스로 어디로 향하는지 잊어버리는 일인지도 모르도다.<br><br>
+    </p>
+    """, unsafe_allow_html=True)
 
-두 순간 사이의 변화는 볼 수 있으나,<br>
-우리가 진정 알고자 했던 것은 ‘바로 이 순간’의 변화였도다.<br><br>
-
-점점 더 가까워지고,<br>
-또 가까워질수록,<br>
-변화율은 하나의 값으로 향하기 시작하니,<br>
-이를 우리는 변화율의 극한이라 부르도다.<br><br>
-
-미분계수란 결국,<br>
-순간 속에 숨어 있는 변화의 방향과 속도를 읽어내려는 시도이며,<br>
-도함수란 그러한 순간들이 이어져 만들어낸 흐름의 기록이 아니겠느냐.
-
-</p>
-""", unsafe_allow_html=True)
-
-st.markdown("---")
-
-# 🖼 슬라이드 1
+with right:
     st.image(
-        f"assets/미분계수와도함수/images/슬라이드1.PNG",
+        "assets/미분계수와도함수/images/슬라이드1.PNG",
         use_container_width=True
     )
+    
 # 좌우 배치
 left, right = st.columns([1, 1.2])
 
