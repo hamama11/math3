61b5ed2cbb859ef15d69ae41122a1b4cf3b7ea5b
hamama11 <ydi74@sonline20.sen.go.kr>
2026-06-01 08:54:49 +0900

    Update 00Ⅱ-4도함수활용(1).py

--- pages/00Ⅱ-4도함수활용(1).py
+++ pages/00Ⅱ-4도함수활용(1).py
@@ -3,8 +3,21 @@ import streamlit.components.v1 as components
 
 st.set_page_config(layout="wide")
 
+
 # =========================
-# 슬라이드 1~7
+# HTML 출력 함수
+def show_html(path, title=None, height=600):
+    if title:
+        st.subheader(title)
+
+    with open(path, "r", encoding="utf-8") as f:
+        html = f.read()
+
+    components.html(html, height=height, scrolling=True)
+
+
+# =========================
+# 슬라이드 1~3
 for i in range(1, 4):
     st.image(
         f"assets/4 도함수활용(1)/images/슬라이드{i}.PNG",
@@ -12,15 +25,14 @@ for i in range(1, 4):
     )
 
 
-
 # =========================
 # HTML
-
 show_html(
     "assets/4 도함수활용(1)/html/mvt.html",
     title="MVT",
     height=600
 )
+
 
 # =========================
 # 슬라이드 4~7
@@ -28,7 +40,4 @@ for i in range(4, 8):
     st.image(
         f"assets/4 도함수활용(1)/images/슬라이드{i}.PNG",
         use_container_width=True
-    )
-
-         height=900
     )
