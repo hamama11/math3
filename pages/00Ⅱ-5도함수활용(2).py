47d90e8ef449be07d5b4cfc912232fe8cb268a63
hamama11 <ydi74@sonline20.sen.go.kr>
2026-06-08 10:44:51 +0900

    Update 00Ⅱ-5도함수활용(2).py

--- pages/00Ⅱ-5도함수활용(2).py
+++ pages/00Ⅱ-5도함수활용(2).py
@@ -5,13 +5,6 @@ st.set_page_config(layout="wide")
 
 
 # =========================
-# 슬라이드 1
-st.image(
-    "assets/5 도함수활용(2)/images/슬라이드1.PNG",
-    use_container_width=True
-)
-
-# =========================
 # HTML 출력 함수
 def show_html(path, title=None, height=600):
     if title:
@@ -24,8 +17,15 @@ def show_html(path, title=None, height=600):
 
 
 # =========================
-# 왼쪽, 오른쪽
+# 슬라이드 1
+st.image(
+    "assets/5 도함수활용(2)/images/슬라이드1.PNG",
+    use_container_width=True
+)
 
+
+# =========================
+# 슬라이드 2 + HTML
 left, right = st.columns([1, 1])
 
 with left:
@@ -37,35 +37,37 @@ with left:
 with right:
     with st.expander("🔍 합성함수그래프", expanded=False):
         show_html(
-        "assets/5 도함수활용(2)/html/합성함수그래프개형.html",
-        height=600
-    )
+            "assets/5 도함수활용(2)/html/합성함수그래프개형.html",
+            height=600
+        )
+
 
 # =========================
-# 슬라이드 3~7
-for i in range(3, 8):
+# 슬라이드 3~6
+for i in range(3, 7):
     st.image(
         f"assets/5 도함수활용(2)/images/슬라이드{i}.PNG",
         use_container_width=True
     )
 
+
 # =========================
 # 중간 문구
-
 st.markdown("""
 <p style='font-size:18px; line-height:1.9;'>
 
 겉으로 드러난 모양만을 보지 말라.<br>
-<b> 참으로 살필 것은 그 안에서 일어나는 변화이니,</b><br>
+<b>참으로 살필 것은 그 안에서 일어나는 변화이니,</b><br>
 찰나에 가까워질수록,<br>
 도함수를 통하여 내적 변화를 읽어 보라.
 </p>
 """, unsafe_allow_html=True)
 
 st.markdown("---")
-# =========================
-# 왼쪽, 오른쪽
 
+
+# =========================
+# 슬라이드 7 + 지오지브라
 left, right = st.columns([1, 1])
 
 with left:
@@ -75,14 +77,20 @@ with left:
     )
 
 with right:
-    with st.expander("📌 지오지브라 열기"):
+    with st.expander("📌 지오지브라 열기", expanded=False):
         components.iframe(
-            https://www.geogebra.org/calculator/vmv32qkk,
+            src="https://www.geogebra.org/calculator/vmv32qkk",
             height=650,
             scrolling=True
         )
-# =========================      
-# 슬라이드 
+
+        st.markdown(
+            "[새 창에서 열기](https://www.geogebra.org/calculator/vmv32qkk)"
+        )
+
+
+# =========================
+# 슬라이드 8
 st.image(
     "assets/5 도함수활용(2)/images/슬라이드8.PNG",
     use_container_width=True
